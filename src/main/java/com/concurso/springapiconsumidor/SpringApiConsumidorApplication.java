2ba4ed7b4fc9652d80b53fcb75a302af200ebda3
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-08 16:37:40 -0300

    Criação do Projeto

---
+++ src/main/java/com/concurso/springapiconsumidor/SpringApiConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.concurso.springapiconsumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiConsumidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiConsumidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurso/springapiconsumidor/SpringApiConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurso.springapiconsumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiConsumidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
