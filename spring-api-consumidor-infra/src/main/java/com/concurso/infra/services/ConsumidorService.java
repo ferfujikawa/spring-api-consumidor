5ec4ce96f423c398d93bc3202723e10ed467840e
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 14:12:05 -0300

    Correção do método de obtenção de hora do servidor.

--- spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConsumidorService.java
+++ spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConsumidorService.java
@@ -1,9 +1,11 @@
 package com.concurso.infra.services;
 
+import java.nio.charset.Charset;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +13,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -23,6 +26,10 @@ import com.concurso.dominio.services.IConsumidorService;
 public class ConsumidorService implements IConsumidorService {
 
     private String wsUrl;
+	
+	private String wsUsuario;
+	
+	private String wsSenha;
     
     private String wsEndpointListarCidade;
 
@@ -32,22 +39,32 @@ public class ConsumidorService implements IConsumidorService {
 
     private String wsEndpointObterHoraDoServidor;
 
-    private HttpHeaders cabecalhoAutenticado;
-
     public ConsumidorService(
         @Value("${ws.url}") String wsUrl,
+        @Value("${ws.usuario}") String wsUsuario,
+        @Value("${ws.senha}") String wsSenha,
         @Value("${ws.endpoints.listarcidades}") String wsEndpointListarCidade,
         @Value("${ws.endpoints.listarpaises}") String wsEndpointListarPaises,
         @Value("${ws.endpoints.obterhoradoservidor}") String wsEndpointObterHoraDoServidor,
-        RestTemplate restTemplate,
-        HttpHeaders cabecalhoAutenticado) {
+        RestTemplate restTemplate) {
         this.wsUrl = wsUrl;
+        this.wsUsuario = wsUsuario;
+        this.wsSenha = wsSenha;
         this.wsEndpointListarCidade = wsEndpointListarCidade;
         this.wsEndpointListarPaises = wsEndpointListarPaises;
         this.wsEndpointObterHoraDoServidor = wsEndpointObterHoraDoServidor;
         this.restTemplate = restTemplate;
-        this.cabecalhoAutenticado = cabecalhoAutenticado;
     }
+
+    private HttpHeaders criarCabecalhoRequisicao() {
+		
+		String auth = wsUsuario + ":" + wsSenha;
+        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+        HttpHeaders header = new HttpHeaders();
+		
+        header.setBasicAuth(new String(encodedAuth));
+		return header;
+	}
 
     @Override
     public List<CidadeDTO> listarCidades(String pesquisa) {
@@ -78,10 +95,13 @@ public class ConsumidorService implements IConsumidorService {
     @Override
     public LocalDateTime obterHoraDoServidor() {
         
-		ResponseEntity<ZonedDateTime> response = restTemplate.getForEntity(
+        HttpHeaders header = criarCabecalhoRequisicao();
+
+        ResponseEntity<ZonedDateTime> response = restTemplate.exchange(
 				wsUrl + wsEndpointObterHoraDoServidor,
-                ZonedDateTime.class,
-				new HttpEntity<String>(cabecalhoAutenticado));
+				HttpMethod.GET,
+				new HttpEntity<String>(header),
+				ZonedDateTime.class);
 		
 		ZonedDateTime dataHoraComTimeZone = response.getBody().withZoneSameInstant(ZoneId.of("America/Sao_Paulo"));
 		return dataHoraComTimeZone.toLocalDateTime();
