26833ddb72e0f51e576c6ac9d735d7ee4bbb48f3
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 13:44:03 -0300

    Criação de bean para injeção de HttpHeader com basic authentication.

--- spring-api-consumidor-infra/src/main/java/com/concurso/infra/configs/RestTemplateConfiguration.java
+++ spring-api-consumidor-infra/src/main/java/com/concurso/infra/configs/RestTemplateConfiguration.java
@@ -1,23 +1,38 @@
 package com.concurso.infra.configs;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.Base64;
 
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
+
+    private String wsUsuario;
+    private String wsSenha;
+
+    public RestTemplateConfiguration(
+        @Value("${ws.usuario}") String wsUsuario,
+        @Value("${ws.senha}") String wsSenha) {
+            
+        this.wsUsuario = wsUsuario;
+        this.wsSenha = wsSenha;
+    }
 
     @Bean
 	RestTemplate restTemplate()
@@ -35,5 +50,16 @@ public class RestTemplateConfiguration {
 		requestFactory.setConnectTimeout(150000);
 
 		return new RestTemplate(requestFactory);
-   }
+    }
+
+    @Bean
+    HttpHeaders cabecalhoAutenticado() {
+
+        String auth = this.wsUsuario + ":" + this.wsSenha;
+        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+        HttpHeaders header = new HttpHeaders();
+		
+        header.setBasicAuth(new String(encodedAuth));
+		return header;
+    }
 }
