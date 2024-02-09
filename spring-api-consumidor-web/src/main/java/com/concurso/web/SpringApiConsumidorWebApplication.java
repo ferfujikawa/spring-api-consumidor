01ade5785fcc248ed6276be5055554446b5d71ba
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 09:05:06 -0300

    Configuração da classe inicializadora para buscar pacotes em com.concurso.

--- spring-api-consumidor-web/src/main/java/com/concurso/web/SpringApiConsumidorWebApplication.java
+++ spring-api-consumidor-web/src/main/java/com/concurso/web/SpringApiConsumidorWebApplication.java
@@ -3,7 +3,7 @@ package com.concurso.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.concurso")
 public class SpringApiConsumidorWebApplication {
 
 	public static void main(String[] args) {
