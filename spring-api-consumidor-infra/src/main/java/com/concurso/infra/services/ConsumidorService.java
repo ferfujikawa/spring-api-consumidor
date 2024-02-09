221b471a50d5410e285bcef0765bf7ded7f09b32
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 11:17:27 -0300

    Implementação de carregamento de países na view de cadastro de concurso.

--- spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConsumidorService.java
+++ spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConsumidorService.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
