a65cfb59b484a6c53700359e3b2d7d21f8ba8f5e
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 14:12:41 -0300

    Utilização da hora obtida do servidor para preenchimento da data de cadastro do concurso.

--- spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConcursoService.java
+++ spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConcursoService.java
@@ -7,14 +7,21 @@ import org.springframework.stereotype.Service;
 import com.concurso.dominio.dtos.CadastroConcursoDTO;
 import com.concurso.dominio.dtos.ConcursoDTO;
 import com.concurso.dominio.services.IConcursoService;
+import com.concurso.dominio.services.IConsumidorService;
 
 @Service
 public class ConcursoService implements IConcursoService {
 
+    private IConsumidorService consumidorService;
+
+    public ConcursoService(IConsumidorService consumidorService) {
+        this.consumidorService = consumidorService;
+    }
+
     @Override
     public ConcursoDTO cadastrarConcurso(CadastroConcursoDTO cadastroConcursoDTO) {
         
-        LocalDateTime dataHoraServidor = LocalDateTime.now();
+        LocalDateTime dataHoraServidor = consumidorService.obterHoraDoServidor();
 		
 		return new ConcursoDTO(
             cadastroConcursoDTO.getNome(),
