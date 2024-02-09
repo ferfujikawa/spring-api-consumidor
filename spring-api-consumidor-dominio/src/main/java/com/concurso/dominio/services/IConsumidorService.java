f4b0f987ac5b052d75b16fc209f8236c58342ceb
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 11:00:41 -0300

    Criação do método para listar países em ConsumidorService.

--- spring-api-consumidor-dominio/src/main/java/com/concurso/dominio/services/IConsumidorService.java
+++ spring-api-consumidor-dominio/src/main/java/com/concurso/dominio/services/IConsumidorService.java
@@ -3,8 +3,11 @@ package com.concurso.dominio.services;
 import java.util.List;
 
 import com.concurso.dominio.dtos.CidadeDTO;
+import com.concurso.dominio.dtos.PaisDTO;
 
 public interface IConsumidorService {
 
     List<CidadeDTO> listarCidades(String pesquisa);
+
+    List<PaisDTO> listarPaises(String pesquisa);
 }
--- spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConsumidorService.java
+++ spring-api-consumidor-infra/src/main/java/com/concurso/infra/services/ConsumidorService.java
@@ -5,32 +5,43 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.concurso.dominio.dtos.CidadeDTO;
+import com.concurso.dominio.dtos.PaisDTO;
 import com.concurso.dominio.services.IConsumidorService;
 
 @Service
 public class ConsumidorService implements IConsumidorService {
 
-    @Value("${ws.url}")
-	private String wsUrl;
+    private String wsUrl;
 	
-	@Value("${ws.usuario}")
 	private String wsUsuario;
 	
-	@Value("${ws.senha}")
 	private String wsSenha;
-
-    @Value("${ws.endpoints.listarcidades}")
+    
     private String wsEndpointListarCidade;
+
+    private String wsEndpointListarPaises;
 
     private RestTemplate restTemplate;
 
-    public ConsumidorService(RestTemplate restTemplate) {
+    public ConsumidorService(
+        @Value("${ws.url}") String wsUrl,
+        @Value("${ws.usuario}") String wsUsuario,
+        @Value("${ws.senha}") String wsSenha,
+        @Value("${ws.endpoints.listarcidades}") String wsEndpointListarCidade,
+        @Value("${ws.endpoints.listarpaises}") String wsEndpointListarPaises,
+        RestTemplate restTemplate) {
+        this.wsUrl = wsUrl;
+        this.wsUsuario = wsUsuario;
+        this.wsSenha = wsSenha;
+        this.wsEndpointListarCidade = wsEndpointListarCidade;
+        this.wsEndpointListarPaises = wsEndpointListarPaises;
         this.restTemplate = restTemplate;
     }
 
@@ -44,6 +55,19 @@ public class ConsumidorService implements IConsumidorService {
             CidadeDTO[].class,
             params);
             
+		return Arrays.asList(response.getBody());
+    }
+
+    @Override
+    public List<PaisDTO> listarPaises(String pesquisa) {
+        
+        Map<String, String> params = Collections.singletonMap("pesquisa", pesquisa);
+		
+		ResponseEntity<PaisDTO[]> response = restTemplate.getForEntity(
+				wsUrl + wsEndpointListarPaises + "?query={pesquisa}",
+				PaisDTO[].class,
+				params);
+		
 		return Arrays.asList(response.getBody());
     }
 }
