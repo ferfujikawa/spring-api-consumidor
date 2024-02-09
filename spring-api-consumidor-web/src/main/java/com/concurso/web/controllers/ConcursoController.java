2eab71fd9d3b043045d83afcffe8ea58994f8816
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 13:27:12 -0300

    Criação da view de confirmação de cadastro de concurso e adição de endpoint POST para salvar concurso.

--- spring-api-consumidor-web/src/main/java/com/concurso/web/controllers/ConcursoController.java
+++ spring-api-consumidor-web/src/main/java/com/concurso/web/controllers/ConcursoController.java
@@ -1,16 +1,57 @@
 package com.concurso.web.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.concurso.dominio.dtos.CadastroConcursoDTO;
+import com.concurso.dominio.dtos.ConcursoDTO;
+import com.concurso.dominio.services.IConcursoService;
+
 
 
 @Controller
 @RequestMapping("concursos")
 public class ConcursoController {
     
-    @GetMapping("/novo")
-    public String exibirFormularioCadastroConcurso() {
+    private IConcursoService concursoService;
+
+    public ConcursoController(IConcursoService concursoService) {
+        this.concursoService = concursoService;
+    }
+
+    @GetMapping("novo")
+    public String exibirFormularioCadastroConcurso(@ModelAttribute("concurso") CadastroConcursoDTO concursoDTO) {
         return "concursos/novo";
     }
+
+    @GetMapping("confirmacao-cadastro")
+    public String getMethodName(@ModelAttribute("concursoCadastrado") ConcursoDTO concursoCadastrado) {
+        return "concursos/confirmacao-cadastro";
+    }
+
+    @PostMapping
+	public String cadastrarConcurso(
+			final Model model,
+			@ModelAttribute("concurso") @Valid CadastroConcursoDTO dto,
+			BindingResult bindingResult,
+            final RedirectAttributes redirectAttributes) {
+		
+        if (bindingResult.hasErrors()) {
+			return exibirFormularioCadastroConcurso(dto);
+		}
+		
+		ConcursoDTO concursoCadastrado = concursoService.cadastrarConcurso(dto);
+
+        redirectAttributes.addFlashAttribute("concursoCadastrado", concursoCadastrado);
+		
+		return "redirect:/concursos/confirmacao-cadastro";
+	}
 }
