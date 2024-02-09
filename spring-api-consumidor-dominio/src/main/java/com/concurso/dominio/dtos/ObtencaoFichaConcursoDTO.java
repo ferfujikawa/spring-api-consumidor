e6634e2ca3e4c4b247fcd8a8228a316c0b311d29
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 14:44:03 -0300

    Inclusão de mensagens de erro de validação.

--- spring-api-consumidor-dominio/src/main/java/com/concurso/dominio/dtos/ObtencaoFichaConcursoDTO.java
+++ spring-api-consumidor-dominio/src/main/java/com/concurso/dominio/dtos/ObtencaoFichaConcursoDTO.java
@@ -1,12 +1,14 @@
 package com.concurso.dominio.dtos;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class ObtencaoFichaConcursoDTO {
 
     @NotNull(message = "Informe o id da inscrição para ")
-	@Min(value = 1, message = "Id deve ser maior ou igual a 1")
+	@Min(value = 1, message = "Id deve ser entre 1 e 68")
+	@Max(value = 68, message = "Id deve ser entre 1 e 68")
 	private Long idInscricao;
 
 	public Long getIdInscricao() {
